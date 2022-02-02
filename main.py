3fdb8bbf3e95439ba5b84c259a7665759c995acd
Claire Daugherty <98245504+claire-md@users.noreply.github.com>
2022-02-02 12:14:25 -0500

    Used .env to store information and fixed a bug.

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import praw
 import os
 from praw.models import Message
+from dotenv import load_dotenv
+
+# Using a .env file to store credentials
+load_dotenv(override=True)
+CLIENT_ID = os.getenv("ID")
+CLIENT_SECRET = os.getenv("SECRET")
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
 
 reddit = praw.Reddit(
     client_id = CLIENT_ID,
     client_secret = CLIENT_SECRET, 
     username = USERNAME,
     password = PASSWORD,
-    user_agent = ""
+    user_agent = "messengerv1 by /u/"
 )
 
 inbox = reddit.inbox.stream()
@@ -16,4 +24,6 @@ inbox = reddit.inbox.stream()
 for message in inbox:
     if not message.was_comment and isinstance(message, Message):
         print(f"From {message.author}: {message.body}")
-        message.reply("This is coming from an API.")
+        message.reply("Hi")
+        # Marks the messages as read so it doesn't resend a message to the same user
+        message.mark_read()
