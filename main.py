11f58b3003d61971063988ef042ad4d03229366e
Claire Daugherty <98245504+claire-md@users.noreply.github.com>
2022-02-09 13:06:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,27 +3,31 @@ import os
 from praw.models import Message
 from dotenv import load_dotenv
 
-# Using a .env file to store credentials
-load_dotenv(override=True)
-CLIENT_ID = os.getenv("ID")
-CLIENT_SECRET = os.getenv("SECRET")
-USERNAME = os.getenv("USERNAME")
-PASSWORD = os.getenv("PASSWORD")
+def main():
+    # Using a .env file to store credentials
+    load_dotenv(override=True)
+    CLIENT_ID = os.getenv("ID")
+    CLIENT_SECRET = os.getenv("SECRET")
+    USERNAME = os.getenv("USERNAME")
+    PASSWORD = os.getenv("PASSWORD")
 
-reddit = praw.Reddit(
-    client_id = CLIENT_ID,
-    client_secret = CLIENT_SECRET, 
-    username = USERNAME,
-    password = PASSWORD,
-    user_agent = "messengerv1 by /u/"
-)
+    reddit = praw.Reddit(
+        client_id = CLIENT_ID,
+        client_secret = CLIENT_SECRET, 
+        username = USERNAME,
+        password = PASSWORD,
+        user_agent = "messengerv1 by /u/"
+    )
 
-inbox = reddit.inbox.stream()
+    inbox = reddit.inbox.stream()
 
+    for message in inbox:
+        if isinstance(message, Message) and not message.was_comment:
+            print(f"From {message.author}: {message.body}")
+            message.reply("Hi")
+            # Marks the messages as read so it doesn't resend a message to the same user
+            message.mark_read()
 
-for message in inbox:
-    if not message.was_comment and isinstance(message, Message):
-        print(f"From {message.author}: {message.body}")
-        message.reply("Hi")
-        # Marks the messages as read so it doesn't resend a message to the same user
-        message.mark_read()
+            
+if __name__ == "__main__":
+    main()
